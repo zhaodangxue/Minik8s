4cba506fdde92c8289506e59340e27e865e5243d
varia <varia07@sjtu.edu.cn>
2024-04-28 22:02:46 +0800

    feat(topics): add topic

--- global/topics.go
+++ global/topics.go
@@ -1,15 +1,27 @@
 package global
 
-const podUpdateTopic = "pod-update"
-const podStatusTopic = "pod-status"
-const testTopic = "test"
+import "minik8s/apiobjects"
 
-func PodUpdateTopic(name string) string {
-	return podUpdateTopic + "-" + name
+const (
+	podUpdateTopic = "pod-update" // 已经存在的Pod的内部信息更新
+	podStatusTopic = "pod-status" // Pod发生增减的时候，通知其他组件
+	testTopic      = "test"       // 测试
+	bindingTopic   = "binding"    // Pod和Node的绑定
+	serviceTopic   = "service"    // Service的增删改查
+)
+
+func PodUpdateTopic(pod apiobjects.Pod) string {
+	return podUpdateTopic + "-" + pod.Name
 }
 func PodStatusTopic() string {
 	return podStatusTopic
 }
 func TestTopic() string {
 	return testTopic
+}
+func BindingTopic() string {
+	return bindingTopic
+}
+func ServiceTopic() string {
+	return serviceTopic
 }
