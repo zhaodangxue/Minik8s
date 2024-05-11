59189e1601ae20420142c1a780b4fb87d6f7079c
varia <varia07@sjtu.edu.cn>
2024-05-11 22:17:38 +0800

    tidy(global): 删除多余的topic

--- global/topics.go
+++ global/topics.go
@@ -1,27 +1,15 @@
 package global
 
-import "minik8s/apiobjects"
-
 const (
-	podUpdateTopic          = "pod-update"           // 已经存在的Pod的内部信息更新
-	podStatusTopic          = "pod-status"           // Pod发生增减的时候，通知其他组件
-	podStateTopic 		 	= "pod-state"            // Pod状态变化
+	podStateTopic 		 	= "pod-state"            // 专门用于发布Pod状态变化
 	testTopic               = "test"                 // 测试
 	bindingTopic            = "binding"              // Pod和Node的绑定
 	serviceTopic            = "service"              // Service的增删改查
 	strategyUpdateTopic     = "strategy-update"      // 策略更新
-	schedulerPodUpdateTopic = "scheduler-pod-update" // Scheduler更新Pod信息
 	podRelevantTopic        = "pod-relevant"         // Pod相关的信息
 	pvRelevantTopic         = "pv-relevant"          // PV相关的信息
 	pvcRelevantTopic        = "pvc-relevant"         // PVC相关的信息
 )
-
-func PodUpdateTopic(pod apiobjects.Pod) string {
-	return podUpdateTopic + "-" + pod.Name + "-" + pod.Namespace
-}
-func PodStatusTopic() string {
-	return podStatusTopic
-}
 func TestTopic() string {
 	return testTopic
 }
@@ -34,10 +22,6 @@ func ServiceTopic() string {
 func StrategyUpdateTopic() string {
 	return strategyUpdateTopic
 }
-func SchedulerPodUpdateTopic() string {
-	return schedulerPodUpdateTopic
-}
-
 func PodStateTopic() string {
 	return podStateTopic
 }
