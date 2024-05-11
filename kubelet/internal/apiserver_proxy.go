024f38a68b7a7c3ae1dd88ad82712d9fd3f09a04
varia <varia07@sjtu.edu.cn>
2024-05-11 22:12:04 +0800

    fix(kubelet): 使用专门的pod state update接口

--- kubelet/internal/apiserver_proxy.go
+++ kubelet/internal/apiserver_proxy.go
@@ -1,15 +1,14 @@
 package internal
 
 import (
-	"minik8s/apiobjects"
 	"minik8s/apiserver/src/route"
 	"minik8s/utils"
 )
 
-func SendPodStatus(pods []*apiobjects.Pod) (err error) {
+func SendPodStatus(pods []*PodWrapper) (err error) {
 	// Send changed pods to apiserver
 	for _, pod := range pods {
-		utils.PutWithJson(route.Prefix + route.NodePath, pod)
+		utils.PutWithJson(route.Prefix + route.PodStatePath, pod.Pod)
 	}
 
 	return
