a90bcdd713b400e0985a931da0508c7a7e29b232
诸柏铭 <18817608661@163.com>
2024-05-23 00:34:48 +0800

    debug replicaset

--- controller/src/registry/controller_list.go
+++ controller/src/registry/controller_list.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"minik8s/controller/api"
 	"minik8s/controller/src/PVcontroller"
+	replicasetcontroller "minik8s/controller/src/ReplicasetController"
 	"minik8s/controller/src/health"
 	"minik8s/controller/src/node"
 )
@@ -11,4 +12,5 @@ var ControllerList []api.Controller = []api.Controller{
 	&health.HealthController{},
 	&node.NodeController{},
 	&PVcontroller.PVcontroller{},
+	&replicasetcontroller.ReplicasetController{},
 }
