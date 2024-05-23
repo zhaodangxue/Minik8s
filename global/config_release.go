d903fd9b4abd684b1754570fe7fa4d2464430784
诸柏铭 <18817608661@163.com>
2024-05-23 21:44:05 +0800

    111

--- global/config_release.go
+++ global/config_release.go
@@ -7,7 +7,7 @@ const Host = "192.168.1.12"
 const ApiVersion = "v1"
 const NFSdir = "/home/nfs"
 const ApiserverMountDir = "/mount/nfs"
-const WorkerMountDir = "/home/.k8s-volume"
+const WorkerMountDir = "/mount/.k8s-volume"
 const Nfsserver = "192.168.1.14"
 
 // 系统命名空间，用于存放系统级别的资源。例如：Node、Binding等
