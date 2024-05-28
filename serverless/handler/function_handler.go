f584b5b0608a3f031ab8f553bf0ddf26ee22aeb2
shi931099 <10940172+shi931099@user.noreply.gitee.com>
2024-05-28 16:51:46 +0800

    finish function and watch handler

--- serverless/handler/function_handler.go
+++ serverless/handler/function_handler.go
@@ -1,9 +1,26 @@
 package serverless_handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"minik8s/apiobjects"
+	"minik8s/global"
+	"minik8s/utils"
+
+	"github.com/gin-gonic/gin"
+)
 
 func FunctionHandler(c *gin.Context) {
+	name := c.Param("name")
+	svcUrl := global.DefaultNamespace + "/" + "function-"+name+"-service"
+	svc := apiobjects.Service{}
+	err := utils.GetUnmarshal("http://localhost:8080/api/get/oneservice/"+svcUrl, &svc)
+	if err != nil {
+		fmt.Println("error")
+	}
+	response,err := utils.PostWithJson(svc.Status.ClusterIP+":8080", svc)
+
 	c.JSON(200, gin.H{
 		"message": "FunctionHandler",
+		"response": response,
 	})
 }
--- serverless/handler/watch_handler.go
+++ serverless/handler/watch_handler.go
@@ -1,7 +1,207 @@
 package serverless_handler
 
-import "github.com/redis/go-redis/v9"
+import (
+	"encoding/json"
+	"fmt"
+	"minik8s/apiobjects"
+	"minik8s/apiserver/src/route"
+	"minik8s/utils"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func FunctionHandlerOnWatch(msg *redis.Message) {
+	utils.Info("Function Handler On Watch")
+	topicMessage := &apiobjects.TopicMessage{}
+	err := json.Unmarshal([]byte(msg.Payload), topicMessage)
+	if err != nil {
+		fmt.Println(err)
+	}
+	switch topicMessage.ActionType {
+	case apiobjects.Create:
+		//调用HandleCreate
+		function := &apiobjects.Function{}
+		err2 := json.Unmarshal([]byte(topicMessage.Object), function)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+		functionJson, _ := json.Marshal(function)
+		HandleCreate([]byte(functionJson))
+	case apiobjects.Update:
+		//调用HandleUpdate
+		function := &apiobjects.Function{}
+		err2 := json.Unmarshal([]byte(topicMessage.Object), function)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+		functionJson, _ := json.Marshal(function)
+		HandleUpdate([]byte(functionJson))
+	}
+}
+
+func HandleCreate(data []byte) {
+	//utils.Info("Handle Function Create")
+	function := &apiobjects.Function{}
+	err := json.Unmarshal(data, function)
+	if err != nil {
+		fmt.Println(err)
+	}
+	utils.Info("Create function: ", function)
+
+	labels := map[string]string{
+		"app": "function-"+function.ObjectMeta.Name+ "-label",
+	}
+    
+	// 1. create the replicaset
+	replicaset := apiobjects.Replicaset{
+		Object: apiobjects.Object{
+			TypeMeta: apiobjects.TypeMeta{
+				ApiVersion: "v1",
+				Kind:       "ReplicaSet",
+			},
+			ObjectMeta: apiobjects.ObjectMeta{
+				Name: "function-"+function.ObjectMeta.Name+"-rs",
+				Namespace: function.ObjectMeta.Namespace,
+			},
+		},
+		Spec: apiobjects.ReplicasetSpec{
+			Replicas: function.Spec.MinReplicas,
+			Selector: apiobjects.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: apiobjects.PodTemplate{
+				Metadata: apiobjects.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: apiobjects.PodSpec{
+					Containers: []apiobjects.Container{
+						{
+							Name:  "function-"+function.ObjectMeta.Name+"-container",
+							//serverIp+":5000/"+imageName+":latest"
+							Image: "192.168.1.15:5000/"+function.Status.ImageUrl + ":latest",
+							Ports: []apiobjects.ContainerPort{
+								{
+									Name: "function-port",
+									ContainerPort: 8080,
+								    HostPort: 8080,
+								},
+							},
+
+						},
+					},
+				},
+			},
+			
+		},
+	}
+	url := route.Prefix + route.ReplicasetPath
+	utils.ApplyApiObject(url, replicaset)
+
+	// 2. create the service
+	service := apiobjects.Service{
+		APIVersion: "v1",	
+		Data: apiobjects.MetaData{
+			Name: 	"function-"+function.ObjectMeta.Name+"-service",
+			Namespace: function.ObjectMeta.Namespace,
+		},
+		Spec: apiobjects.ServiceSpec{
+			Selector: labels,
+			Type: apiobjects.ServiceTypeClusterIP,
+			Ports: []apiobjects.ServicePort{
+				{
+					Name: "function-port",
+					Protocol: apiobjects.ProtocolTCP,
+					Port: 8080,
+					TargetPort: "function-port",
+				},
+			},
+		},
+	}
+	url = route.Prefix + route.ServiceApplyPath
+	utils.ApplyApiObject(url, service)
+}
+
+func HandleUpdate(data []byte) {
+	//utils.Info("Handle Function Update")
+	utils.Error("Handle Function Update Don't Support")
+
+	function := &apiobjects.Function{}
+	err := json.Unmarshal(data, function)
+	if err != nil {
+		fmt.Println(err)
+	}
+	utils.Info("Update function: ", function)
+	labels := map[string]string{
+		"app": "function-"+function.ObjectMeta.Name+ "-label",
+	}
+
+		// 1. create the replicaset
+		replicaset := apiobjects.Replicaset{
+			Object: apiobjects.Object{
+				TypeMeta: apiobjects.TypeMeta{
+					ApiVersion: "v1",
+					Kind:       "ReplicaSet",
+				},
+				ObjectMeta: apiobjects.ObjectMeta{
+					Name: "function-"+function.ObjectMeta.Name+"-rs",
+					Namespace: function.ObjectMeta.Namespace,
+				},
+			},
+			Spec: apiobjects.ReplicasetSpec{
+				Replicas: function.Spec.MinReplicas,
+				Selector: apiobjects.LabelSelector{
+					MatchLabels: labels,
+				},
+				Template: apiobjects.PodTemplate{
+					Metadata: apiobjects.ObjectMeta{
+						Labels: labels,
+					},
+					Spec: apiobjects.PodSpec{
+						Containers: []apiobjects.Container{
+							{
+								Name:  "function-"+function.ObjectMeta.Name+"-container",
+								//serverIp+":5000/"+imageName+":latest"
+								Image: "192.168.1.15:5000/"+function.Status.ImageUrl + ":latest",
+								Ports: []apiobjects.ContainerPort{
+									{
+										Name: "function-port",
+										ContainerPort: 8080,
+										HostPort: 8080,
+									},
+								},
+	
+							},
+						},
+					},
+				},
+				
+			},
+		}
+		url := route.Prefix + route.ReplicasetPath
+		utils.ApplyApiObject(url, replicaset)
+	
+		// 2. create the service
+		service := apiobjects.Service{
+			APIVersion: "v1",	
+			Data: apiobjects.MetaData{
+				Name: 	"function-"+function.ObjectMeta.Name+"-service",
+				Namespace: function.ObjectMeta.Namespace,
+			},
+			Spec: apiobjects.ServiceSpec{
+				Selector: labels,
+				Type: apiobjects.ServiceTypeClusterIP,
+				Ports: []apiobjects.ServicePort{
+					{
+						Name: "function-port",
+						Protocol: apiobjects.ProtocolTCP,
+						Port: 8080,
+						TargetPort: "function-port",
+					},
+				},
+			},
+		}
+		url = route.Prefix + route.ServiceApplyPath
+		utils.ApplyApiObject(url, service)
+
 
 }
