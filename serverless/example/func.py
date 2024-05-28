1045e44ceb45b7018bcbcc017e423bbc5c95b6f2
varia <varia07@sjtu.edu.cn>
2024-05-28 23:51:05 +0800

    draft(watchdog): add content to watchdog

---
+++ serverless/example/func.py
@@ -0,0 +1,4 @@
+
+def main(params):
+    return {"params": params,"message": "Hello, World!"}
+
--- serverless/imagebase/imagedata/function/watchdog.py
+++ serverless/imagebase/imagedata/function/watchdog.py
@@ -1,2 +1,22 @@
+import os
+import json
+import func
 
-print("Hello from watchdog.py!")
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST'])
+def work():
+    try:
+        params = json.loads(request.get_data())
+    except json.JSONDecodeError:
+        params = ""
+    finally:
+        res = func.main(params)
+        return json.dumps(res)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
