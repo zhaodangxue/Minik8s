f9190101aab4bc3a6e345306d577e6aa26d3393a
varia <varia07@sjtu.edu.cn>
2024-06-04 20:24:14 +0800

    draft(serverless/demo): 完成demo函数部分

--- serverless/examples/image_process/allinone/upload_image.py
+++ serverless/examples/image_process/allinone/upload_image.py
@@ -1,15 +1,27 @@
 import couchdb
 import os
+import sys
+from uuid import uuid4
 
 def main(params):
     # 从环境变量中获取数据库的用户名和密码
     username = os.environ['COUCHDB_USERNAME']
     password = os.environ['COUCHDB_PASSWORD']
-    # Connect to CouchDB
+    path = params['path']
+    # 连接CouchDB
     couch = couchdb.Server()
-    couch.login(username, password)
+    couch.resource.credentials = (username, password)
+    db = couch['images']
+    # 保存文件到CouchDB
+    with open(path, 'rb') as f:
+        uid = uuid4().hex
+        doc = {"_id":uid}
+        res = db.save(doc)
+        db.put_attachment(doc, f, "image")
 
     return {}
 
 if __name__ == '__main__':
-    main({})
+    # 从参数中获取图片的Path
+    path = "/home/varia/tmp/test-img.png"
+    main({'path': path})
---
+++ serverless/examples/image_process/thumbnail/func.py
@@ -0,0 +1,44 @@
+import couchdb
+import cv2
+import numpy as np
+
+"""
+params:{
+    couchdb_username: "",
+    couchdb_password: "",
+    id: "",
+    new_doc_id: ""
+}
+res:{
+    res_doc_id: ""
+}
+"""
+
+def get_image_from_attachment(db, doc_id, attachment_name):
+    doc = db[doc_id]
+    res = db.get_attachment(doc, attachment_name)
+    img = np.asarray(bytearray(res.read()), dtype="uint8")
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    return img
+
+def func(params):
+    # Connect to CouchDB
+    couch = couchdb.Server()
+    couch.resource.credentials = (params['couchdb_username'], params['couchdb_password'])
+    target_doc_id = params['new_doc_id']
+    db = couch['images']
+
+    # Get the image from CouchDB
+    img = get_image_from_attachment(db, target_doc_id, 'processed_image')
+
+    # Generate thumbnail
+    # Scale till the smaller side is 100
+    scale = 100 / min(img.shape[:2])
+    thumbnail = cv2.resize(img, (0, 0), fx=scale, fy=scale)
+    target_doc = db[target_doc_id]
+    target_doc['thumbnail'] = {'size': thumbnail.shape}
+    db.save(target_doc)
+    data = cv2.imencode('.png', thumbnail)[1].tobytes()
+    db.put_attachment(target_doc, data, 'thumbnail', 'image/png')
+
+    return {'res_doc_id': target_doc_id}
